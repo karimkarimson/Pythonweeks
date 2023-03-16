c2109d913788178a437acb4993653c49e6a33676
Karim Aouini <karim.aouini@docc.techstarter.de>
2023-03-16 17:09:35 +0100

    first py

---
+++ HalloPy/hallopy.py
@@ -0,0 +1,7 @@
+nachricht=4
+nachricht+=3
+print("Nachricht ist derzeit: " , nachricht)
+nachricht="Hello Python"
+print("Jetzt ist die Nachricht: "+nachricht)
+print("Und jetzt wird sie manipuliert zu: "+nachricht.upper())
+
